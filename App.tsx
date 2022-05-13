296d72a9a449ad7d7b5c659a8525b7d53190ae39
aydn-burner <hooperaydn@gmail.com>
2022-05-13 13:10:20 +0200

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>
+        Display user names from ./data/users in a list below this statement
+      </h1>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-8nhs8h
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-8nhs8h)
